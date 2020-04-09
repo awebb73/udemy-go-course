65bf519cd51df1468fd7e4de8552c25148eb8f01
Art Webb <awebb73@gmail.com>
2020-04-09 15:50:39 -0700

    lame ass changes

--- 001-hello-world/main.go
+++ 001-hello-world/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "rsc.io/quote"
+import "fmt"
 
-func Hello() string {
-	return quote.Hello()
+func main() {
+	fmt.Println("Hello everyone, this is the most awesome class in the entire world. We are having fun and learning the GO programming language")
+
+	foo()
+
+	fmt.Println("something more")
+
+	for i := 0; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+
+	bar()
+}
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+func bar() {
+	fmt.Println("and then we exited")
 }
