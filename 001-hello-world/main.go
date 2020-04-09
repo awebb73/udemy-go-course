c9a7a6b1d7034128171eb1e58c0c4e989fb7041e
Art Webb <awebb73@gmail.com>
2020-04-09 16:03:52 -0700

    you are doing great

--- 001-hello-world/main.go
+++ 001-hello-world/main.go
@@ -3,11 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello everyone, this is the most awesome class in the entire world. We are having fun and learning the GO programming language")
+	// n is the number of bytes
+	// err is a write error
+	// both are returned by fmt.Println
+	n, err := fmt.Println("Hello everyone, this is the most awesome class in the entire world. We are having fun and learning the GO programming language")
+
+	fmt.Println("Number of bytes:", n)
+
+	fmt.Println("WRITE ERR:", err)
 
 	foo()
 
-	fmt.Println("something more")
+	// _ is how we let the compiler know we arent goign to use a return value
+	x, _ := fmt.Println("something more")
+
+	fmt.Println("Number of bytes:", x)
 
 	for i := 0; i < 100; i++ {
 		if i%2 == 0 {
